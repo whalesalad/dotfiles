dd80ab75c427659b35d3cfeb92be8f84529edd6b
Michael Whalen <michael@whalesalad.com>
2018-12-17 09:54:35 -0800

    Added ggraph alias to graph current git branch topology.

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -10,3 +10,8 @@ function ggb() {
     refs/heads/ \
     --format='%(align:left,20)%(color:green)%(committerdate:relative)%(color:reset)%(end) %(align:left,50)%(color:yellow)%(refname:short)%(color:reset)%(end) %(color:red)%(objectname:short)%(color:reset) %(contents:subject) (%(authorname))'
 }
+
+function ggraph() {
+  git log --graph --full-history --all --color \
+          --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+}
