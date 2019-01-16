4f68e17a9c81633c4db83689dd9551d8883c23ca
Michael Whalen <michael@whalesalad.com>
2019-01-15 21:16:23 -0800

    Moved all config into base.

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,3 +1,8 @@
+ZSH_THEME="agnoster"
+COMPLETION_WAITING_DOTS="true"
+
+plugins=(git docker rails heroku virtualenv virtualenvwrapper)
+
 UNAME=`uname | tr '[:upper:]' '[:lower:]'`
 
 source "${0:a:h}/${UNAME}.zshrc"
