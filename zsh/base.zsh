e2686ad2c1eecb3f042dc9accf49acd2553a7d41
Michael Whalen <michael@whalesalad.com>
2023-08-08 21:27:36 -0400

    allow for hostname specific zshrc overrides

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -2,6 +2,7 @@ ZSH_THEME="agnoster"
 COMPLETION_WAITING_DOTS="true"
 
 UNAME=`uname | tr '[:upper:]' '[:lower:]'`
+HOSTNAME=`hostname`
 
 # load env
 function lenv() {
@@ -9,3 +10,7 @@ function lenv() {
 }
 
 source "${0:a:h}/${UNAME}.zshrc"
+
+if [[ -f "${0:a:h}/hosts/${HOSTNAME}.zshrc" ]]; then
+  source "${0:a:h}/hosts/${HOSTNAME}.zshrc"
+fi
